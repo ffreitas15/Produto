af6014c4c099ce2414998138ded8cff3a35ecb7f
ffreitas15 <fernandaighorg@gmail.com>
2022-02-16 23:42:40 -0300

    first commit

---
+++ Produto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double valorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void adicionarProdutos(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+
+        public override string ToString()
+        {
+            return Nome + "$" + Preco
+            + ","
+            + Quantidade
+            + " unidades , total : $ "
+            + valorTotalEmEstoque();
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            System.Console.WriteLine("Entre com os dados do produto");
+            System.Console.WriteLine("nome: ");
+
+            p.Nome = Console.ReadLine();
+            System.Console.WriteLine("Digite o Preço:  ");
+            p.Preco = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade no estoque:  ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("dados do protudo" + p);
+
+            System.Console.WriteLine("digete o numero para ser colocado em estoque :");
+            int qte = int.Parse(Console.ReadLine());
+            p.adicionarProdutos(qte);
+            System.Console.WriteLine("Daddos atualizados");
+
+
+            System.Console.WriteLine("digete o numero para ser removido em estoque :");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qte);
+            System.Console.WriteLine("Daddos atualizados" + p);
+        }
+    }
+}
